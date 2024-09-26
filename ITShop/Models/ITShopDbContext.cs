c31295717cc3defb99f86212ed68216f2ad8b353
CaoNgocToan <121233032+CaoNgocToan@users.noreply.github.com>
2024-09-26 15:20:13 +0700

    init project

---
+++ ITShop/Models/ITShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ITShop.Models
+{
+    public class ITShopDbContext : DbContext
+    {
+        public ITShopDbContext(DbContextOptions<ITShopDbContext> options) : base(options) { }
+        public DbSet<LoaiSanPham> LoaiSanPham { get; set; }
+        public DbSet<HangSanXuat> HangSanXuat { get; set; }
+        public DbSet<SanPham> SanPham { get; set; }
+        public DbSet<NguoiDung> NguoiDung { get; set; }
+        public DbSet<TinhTrang> TinhTrang { get; set; }
+        public DbSet<DatHang> DatHang { get; set; }
+        public DbSet<DatHang_ChiTiet> DatHang_ChiTiet { get; set; }
+    }
+}
